900f323ef3a47497e1824237227a03b05865f6d4
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-11-09 11:15:30 +0100

    timer and array + some old stuff

--- projects/test-js/assets/js/4-funktion.js
+++ projects/test-js/assets/js/4-funktion.js
@@ -1,71 +1,17 @@
-// Föräldern sjunger och barnet lyssnar
-// console.log("blinka lilla stjärna där");
-// console.log("blinka lilla stjärna där");
-// console.log("garbage teast");
-// console.log("likt en diamant i skyn");
-
-//barnet nynnar med
-// console.log("blinka lilla stjärna där");
-// console.log("blinka lilla stjärna där");
-// console.log("hur jag undrar var du är");
-// console.log("likt en diamant i skyn");
-
-//Nu sjunger barnet helt själv
-// console.log("blinka lilla stjärna där");
-// console.log("blinka lilla stjärna där");
-// console.log("hur jag undrar var du är");
-// console.log("likt en diamant i skyn");
-
-//THIS CODE IS WET! write everything twice 
-//Eftersträva att et kod är DRY! Don't Repeat Yourself
-function twinkleTwinkle() {
-    console.log("blinka lilla stjärna där");
-    console.log("blinka lilla stjärna där");
-    console.log("hur jag undrar var du är");
-    console.log("fjärran lockar du min syn");
-    console.log("likt en diamant i skyn");
+function myTimer() {
+    var d = new Date();
+    document.getElementById("output").innerHTML = d.toLocaleTimeString();
 }
 
-// Call a funktion - Kalla/anropa på funktion
-twinkleTwinkle();
-twinkleTwinkle();
-twinkleTwinkle();
+myTimer();
 
-//Deklarera en funktion 
-function doSomething() {
-    console.log("Hello World!");
-    console.log("im doing something...")
-}
+var myTime = setInterval(myTimer, 1000);
 
-//Anropa funktion
-doSomething();
-//Visar innehållet i funktionen
-doSomething;
+document.getElementsByTagName("button")[0].addEventListener("click",function(){
+    clearInterval(myTime);
+});
 
-//parameter och argumrnt
-function square(num, num1) {
-    console.log(num * num1);
-}
+document.querySelector("button".addEventListener("dblclick", function() {
+    myTime = setInterval(myTimer, 1000);
 
-square(5,6);
-
-function sayHello(name) {
-    console.log("Well hello there, dear "+ name);
-}
-function greet(person1, person2 ,person3 ,person4) {
-    console.log("Hej, " + person1 + "!")
-    console.log("Hej, " + person2 + "!")
-    console.log("Hej, " + person3 + "!")
-    console.log("Hej, " + person4 + "!")
-}
-
-greet("fam", "yo", "swag", "meme");
-
-function squared(x) {
-    return x * x;
-}
-
-console.log("4 squared is: " + squared(4));
-
-var result = squared(10);
-console.log(result);
+}));
---
+++ projects/test-js/assets/js/5-array.js
@@ -0,0 +1,6 @@
+var friend1 = "Ivy";
+var friend2 = "Klaus";
+var friend3 = "Bob";
+var friend4 = "Mark";
+
+var firends = ["Ivy", "Klaus", "Bob", "Mark"]
