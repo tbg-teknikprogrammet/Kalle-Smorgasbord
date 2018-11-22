0b1e2dbdae1e59a0abbe5ea4aa85363f922b8e31
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-11-22 13:07:35 +0100

    last lesson arrays forgot to push

--- projects/test-js/assets/js/5-array.js
+++ projects/test-js/assets/js/5-array.js
@@ -1,6 +1,41 @@
 var friend1 = "Ivy";
 var friend2 = "Klaus";
-var friend3 = "Bob";
+var friend3 = "Bex";
 var friend4 = "Mark";
 
-var firends = ["Ivy", "Klaus", "Bob", "Mark"]
+var firends = ["Ivy", "Klaus", "Bex", "Mark"];
+
+console.log(firends[2] + " <3 " + friends[1]);
+
+
+//olika typer av array men visar samma sak 
+var color = []; // standard
+var color = new Array(); // rare not common 
+var randomeArray = ["string", 31, true, false];
+
+
+//push()
+
+color = ["red", "green", "blue"];
+console.log(color);
+color.push("purple");
+console.log(color);
+
+//pop()
+color.pop();//tar bort sista värdet
+
+//unshift()
+color.unshift("yellow");
+
+//shift()
+color.shift()
+
+//indexOf()
+
+//slice()
+
+var fruits = ["Bannana", "Orange", "Lemon", "Mango", "Pear"];
+
+
+var citrus = fruits.slice(1, 3);
+console.log(citrus);
