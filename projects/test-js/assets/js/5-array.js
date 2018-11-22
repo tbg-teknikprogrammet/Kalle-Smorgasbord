6c716d369648cbb8995a986a5742884adb8d2e1a
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-11-22 14:04:08 +0100

    never made question

--- projects/test-js/assets/js/5-array.js
+++ projects/test-js/assets/js/5-array.js
@@ -39,3 +39,43 @@ var fruits = ["Bannana", "Orange", "Lemon", "Mango", "Pear"];
 
 var citrus = fruits.slice(1, 3);
 console.log(citrus);
+
+
+var cuntries = ["Sweden", "Norway", "Finland", "Danmark", "Island"]
+cuntries.forEach(function(cuntry){
+    console.log(cuntry);
+});
+
+function countryList(country) {
+    console.log("^^^^^^^^^^^^^^^^");
+    console.log(country);
+    console.log()
+}
+
+
+var x = [1, 2, 3, 4, 5]
+
+x.forEach(function(printReverse){
+console.log(printReverse);
+for (printReverse = x.length; printReverse >= 0; printReverse-1) {
+    console.log(printReverse);  
+}
+});
+
+function printReverse(v) {
+    for(var i = array.length -1; i >= 0; i--) {
+        cpmsp
+
+    }
+}
+var v = [1, 2, 3, 4]
+function v(mirror) {
+    console.log(mirror)
+    
+   if(i === x) {
+       true
+   }
+   else {
+       false
+   }
+}
