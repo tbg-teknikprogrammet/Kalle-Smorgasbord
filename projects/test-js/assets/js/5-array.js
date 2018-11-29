04e3e83cb169a75aacdcbd75331bc0976190599d
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-11-29 13:52:20 +0100

    1111 mirror

--- projects/test-js/assets/js/5-array.js
+++ projects/test-js/assets/js/5-array.js
@@ -1,81 +1,15 @@
-var friend1 = "Ivy";
-var friend2 = "Klaus";
-var friend3 = "Bex";
-var friend4 = "Mark";
 
-var firends = ["Ivy", "Klaus", "Bex", "Mark"];
-
-console.log(firends[2] + " <3 " + friends[1]);
-
-
-//olika typer av array men visar samma sak 
-var color = []; // standard
-var color = new Array(); // rare not common 
-var randomeArray = ["string", 31, true, false];
-
-
-//push()
-
-color = ["red", "green", "blue"];
-console.log(color);
-color.push("purple");
-console.log(color);
-
-//pop()
-color.pop();//tar bort sista värdet
-
-//unshift()
-color.unshift("yellow");
-
-//shift()
-color.shift()
-
-//indexOf()
-
-//slice()
-
-var fruits = ["Bannana", "Orange", "Lemon", "Mango", "Pear"];
-
-
-var citrus = fruits.slice(1, 3);
-console.log(citrus);
-
-
-var cuntries = ["Sweden", "Norway", "Finland", "Danmark", "Island"]
-cuntries.forEach(function(cuntry){
-    console.log(cuntry);
-});
-
-function countryList(country) {
-    console.log("^^^^^^^^^^^^^^^^");
-    console.log(country);
-    console.log()
-}
-
-
-var x = [1, 2, 3, 4, 5]
-
-x.forEach(function(printReverse){
-console.log(printReverse);
-for (printReverse = x.length; printReverse >= 0; printReverse-1) {
-    console.log(printReverse);  
-}
-});
-
-function printReverse(v) {
-    for(var i = array.length -1; i >= 0; i--) {
-        cpmsp
-
-    }
-}
-var v = [1, 2, 3, 4]
-function v(mirror) {
-    console.log(mirror)
+var v = [1, 1, 1, 1]
+function mirror(v) {
+    console.log(v)
     
-   if(i === x) {
-       true
-   }
-   else {
-       false
-   }
+    for(var i = 0; i <= v.length-1; i++) {
+        if(v[0] != v[i]) {
+            console.log("false")
+            return false;
+        }
+    }
+    console.log("true")
+    return true;
 }
+mirror(v)
