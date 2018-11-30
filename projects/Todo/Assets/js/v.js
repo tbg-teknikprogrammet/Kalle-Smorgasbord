7c9e4296883016f4a0ee02ded517991f9bcddd67
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-11-30 11:13:07 +0100

    small start

---
+++ projects/Todo/Assets/js/v.js
@@ -0,0 +1,10 @@
+//fresh
+document.getElementsByTagName("button")[0].addEventListener("click",menu);
+
+function menu() {
+    input = 0
+    while(input != "quit"){
+        input = prompt("Vad vill du modifiera")
+    }
+    if(input == "new")
+}
