8e1b17462927eda14cae17723d0a26bb9389b20c
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-12-04 13:03:43 +0100

    done with ToDo list

--- projects/Todo/Assets/js/v.js
+++ projects/Todo/Assets/js/v.js
@@ -1,10 +1,31 @@
 //fresh
 document.getElementsByTagName("button")[0].addEventListener("click",menu);
 
-function menu() {
-    input = 0
+function menu(vector) {
+    var vector = [];
+    var input = 0;
     while(input != "quit"){
-        input = prompt("Vad vill du modifiera")
+        input = prompt("What do you want to modify")
+    switch (input) {
+        case "new":
+        input = prompt("What do you want to remember?");
+        vector.push(input);
+            break;
+        case "list":
+        for(var i = 0; i <= vector.length-1; i++) 
+            {
+                console.log(vector[i])
+            }
+            break;
+        case "delete":
+            input = prompt("What shit do you want to remove?");
+            vector.splice(input , 1);
+            break;
+        case "quit":
+
+            break;
+        default:
+            Console.writeline("please try again.")
+            break;
     }
-    if(input == "new")
-}
+}}
