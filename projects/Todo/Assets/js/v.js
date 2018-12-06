8118be19c4e2145c1cc41a5f962054ac53c1e424
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-12-06 13:50:41 +0100

    reworked on the next lesson

--- projects/Todo/Assets/js/v.js
+++ projects/Todo/Assets/js/v.js
@@ -6,26 +6,30 @@ function menu(vector) {
     var input = 0;
     while(input != "quit"){
         input = prompt("What do you want to modify")
-    switch (input) {
-        case "new":
-        input = prompt("What do you want to remember?");
-        vector.push(input);
-            break;
-        case "list":
-        for(var i = 0; i <= vector.length-1; i++) 
-            {
-                console.log(vector[i])
-            }
-            break;
-        case "delete":
-            input = prompt("What shit do you want to remove?");
-            vector.splice(input , 1);
-            break;
-        case "quit":
+        switch (input) {
+            case "new":
+            input = prompt("What do you want to remember?");
+            vector.push(input);
+            console.log("you have added: " + input)
+                break;
+            case "list":
+            for(var i = 0; i <= vector.length-1; i++) 
+                {
+                    console.log(vector[i])
+                }
+                console.log("this is all the todo in your todo list")
+                break;
+            case "delete":
+                input = prompt("What shit do you want to remove?");
+                vector.splice(input , 1);
+                console.log("you have deleted index: " + input + " from the list!")
+                break;
+            case "quit":
 
-            break;
-        default:
-            Console.writeline("please try again.")
-            break;
+                break;
+            default:
+                console.writeline("please try again.")
+                break;
     }
+    
 }}
