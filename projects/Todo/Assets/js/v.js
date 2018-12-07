381208dea0e919551f8a3086e14ae402a3c3366c
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2018-12-07 11:13:13 +0100

    cleared all the mistakes fully done

--- projects/Todo/Assets/js/v.js
+++ projects/Todo/Assets/js/v.js
@@ -1,25 +1,25 @@
 //fresh
 document.getElementsByTagName("button")[0].addEventListener("click",menu);
-
-function menu(vector) {
+//in the button click starts the while loop
+function menu(vector) { //runs the function meny and creates a empty array
     var vector = [];
     var input = 0;
     while(input != "quit"){
         input = prompt("What do you want to modify")
-        switch (input) {
-            case "new":
+        switch (input) {// same as if else
+            case "new"://adds item to the end of the array
             input = prompt("What do you want to remember?");
             vector.push(input);
             console.log("you have added: " + input)
                 break;
-            case "list":
+            case "list"://shows all in the array
             for(var i = 0; i <= vector.length-1; i++) 
                 {
                     console.log(vector[i])
                 }
                 console.log("this is all the todo in your todo list")
                 break;
-            case "delete":
+            case "delete"://delets the item on the list via index
                 input = prompt("What shit do you want to remove?");
                 vector.splice(input , 1);
                 console.log("you have deleted index: " + input + " from the list!")
@@ -27,8 +27,8 @@ function menu(vector) {
             case "quit":
 
                 break;
-            default:
-                console.writeline("please try again.")
+            default://same as try catch in C#
+                console.log("please try again.")
                 break;
     }
     
